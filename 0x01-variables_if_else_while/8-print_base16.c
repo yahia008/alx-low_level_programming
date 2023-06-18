4346e8f7d898e92796212f26253540c4fb866cb0
yahia008 <yahyatijjani99@gmail.com>
2023-06-18 16:30:00 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	for (letter = 'a'; letter < 'g'; ++letter)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
