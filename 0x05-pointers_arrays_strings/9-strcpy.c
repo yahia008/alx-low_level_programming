8d726314141c1ccf0a27e6693419fa2e2b0ecdd8
yahia008 <yahyatijjani99@gmail.com>
2023-06-26 12:01:58 -0700

    ki

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strcpy - copy string from one source to a destination
+ * @dest: the destination passed
+ * @src: the source passed
+ * Return: The copy of the string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
