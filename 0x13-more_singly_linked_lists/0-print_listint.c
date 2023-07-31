1b9138a4493966d4248b6f57fc9b7bbdb7e35d7b
yahia008 <yahyatijjani99@gmail.com>
2023-07-31 16:27:05 -0700

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	const listint_t *ptr;
 
-	ptr = h;
-	while (ptr)
+
+
+	while (h)
 	{
-		printf("%d\n", ptr->n);
-		ptr = ptr->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 	return (count);
