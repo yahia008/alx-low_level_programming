6f4e67ed2c6419381702fe802785b29b783a9e63
yahia008 <yahyatijjani99@gmail.com>
2023-08-03 12:50:03 -0700

    first1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - Converts a binary string to an unsigned int.
- * @binary_string: A pointer to a string,
+ * @b: A pointer to a string,
  * containing only '0' and '1' characters.
  *
  * Return: If binary_string is NULL or contains characters,
@@ -14,14 +14,19 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int total = 0,  baseVal = 1;
 	int biLen = strlen(b);
+	int i;
 
-	for (int i = (biLen - 1); i >= 0; i--)
+	if (b == NULL)
+		return (0);
+
+	for (i = (biLen - 1); i >= 0; i--)
 	{
-        if (string[i] == '1') total += baseVal;
-		baseVal *=2;
+		if (b[i] == '1')
+		total += baseVal;
+		baseVal *= 2;
+
 	}
 
-	return total;
-
+	return (total);
 
 }
