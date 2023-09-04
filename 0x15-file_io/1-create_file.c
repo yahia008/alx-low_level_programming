667df63cfd58b4313652b38b4354c0f9c795a3bf
yahia008 <yahyatijjani99@gmail.com>
2023-09-04 06:50:16 -0700

    jones

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 			;
 	}
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file_descriptor == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 	bytes_written = write(fd, text_content, content_len);
 
